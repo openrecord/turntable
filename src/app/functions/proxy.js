0b3e7fe66417d26c259c70ab937724a67b00e864
Chas DeVeas <chas@videoblocks.com>
2018-05-26 13:38:39 -0500

    [Offline] Close db connections for offline use.

--- src/app/functions/proxy.js
+++ src/app/functions/proxy.js
@@ -1,9 +1,15 @@
 const RequestFactory = require('../modules/common/RequestFactory')
+const db = require('../services/database')
 const server = require('../server')
 
 module.exports = async (event, context, callback) => {
   const request = RequestFactory.fromLambdaEvent(event)
   const response = await server.inject(request)
+
+  if (process.env.IS_OFFLINE) {
+    console.log('Closing db')
+    await db.close()
+  }
 
   return {
     headers: response.headers,
