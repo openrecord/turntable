90b01349415e55e15ba72abf83343f7bc4d0acb7
Chas DeVeas <chas@videoblocks.com>
2018-06-04 22:17:56 -0400

    .

--- .circleci/synchronizeDatabase.js
+++ .circleci/synchronizeDatabase.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const {connect} = require('../src/app/services/database')
-
 if (process.env.NODE_ENV !== 'test') {
   console.log('Overriding NODE_ENV to "test"')
   process.env.NODE_ENV = 'test'
 }
+
+const {connect} = require('../src/app/services/database')
 
 async function sync() {
   const conn = await connect()
