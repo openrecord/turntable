633bde3c583821f978e5d133c704d025fbc7f3df
Chas DeVeas <chas@videoblocks.com>
2018-07-15 22:24:47 -0400

    [CircleCi] Fix build_tag_push.sh to use sh for CircleCi docker image compatibility.

--- docker/api/build_tag_push.sh
+++ docker/api/build_tag_push.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 IMAGE=openrecord/turntable
 
@@ -6,7 +6,7 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd ${PROJECT_ROOT}
 
 echo "Building Docker image: [Image: $IMAGE]"
-docker build . --file docker/turntable/Dockerfile -t ${IMAGE}
+docker build . --file docker/api/Dockerfile -t ${IMAGE}
 
 echo "Tagging image as latest: [Image: $IMAGE, Tag: $IMAGE:latest]"
 docker tag ${IMAGE} ${IMAGE}:latest
