3cbdd2b0796e56aa9bc3b8107112f94e50d25429
jamesmcnamara <jamesscottmcnamara@gmail.com>
2018-11-30 02:07:10 -0800

    reverted compilation to webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './functions/graphql.ts',
+  output: {
+    filename: 'graphql.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-typescript']
+          }
+        }
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader'
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts']
+  },
+  node: {
+    module: 'empty',
+    fs: 'empty'
+  }
+};
